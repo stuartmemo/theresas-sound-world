ff2e5f10eff493670ea239d7a6eee45365098603
Stuart Memo <stuartmemo@gmail.com>
2013-04-09 22:13:10 +0100

    grunt reorg

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function (grunt) {
+    'use strict';
+
+    // Project config.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+            all: {
+                files: {
+                    'build/tsw.min.js': ['src/tsw.js']
+                },
+                options: {
+                    banner: '/* <%= pkg.title %> <%= pkg.version %> (c) 2013 Stuart Memo %> */\n'
+                }
+            }
+        }
+    });
+
+    // Load plugins.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default tasks.
+    grunt.registerTask('default', ['uglify']);
+};
