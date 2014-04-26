ecfedf82c3e99a8aebffd495a07e52b5d10cbaf5
Jack Anderson <jackcanderson@gmail.com>
2014-04-26 12:07:22 -0700

    .vimrc for unique tab/space setup + fix test.js
    - the softtabs of 4 spaces is somewhat unique so setting this option
    helps those who use vim
    - fixed spacing in test.js

--- tests/tests.js
+++ tests/tests.js
@@ -5,35 +5,34 @@
 
 describe('Theresa\'s Sound World', function () {
     'use strict';
-  
-	describe('Core', function () {
 
-		describe('Audio Context', function () {
+    describe('Core', function () {
 
-			it('Context exists', function () {
-				expect(typeof tsw.context).toEqual('function');
-			});
-		});
+        describe('Audio Context', function () {
 
-		describe('Output', function () {
+            it('Context exists', function () {
+                expect(typeof tsw.context).toEqual('function');
+            });
+        });
 
-			it('Speakers exist', function () {
-				expect(typeof tsw.speakers).toEqual('object');
-			});
-		});
+        describe('Output', function () {
 
-		describe('Time', function () {
+            it('Speakers exist', function () {
+                expect(typeof tsw.speakers).toEqual('object');
+            });
+        });
 
-			it('Now is current context time', function () {
-				expect(tsw.context().currentTime).toEqual(tsw.now());
-			});
-		});
+        describe('Time', function () {
+            it('Now is current context time', function () {
+                expect(tsw.context().currentTime).toEqual(tsw.now());
+            });
+        });
 
-		describe('Nodes', function () {
-			it('Can create node', function () {
-				expect(tsw.createNode().nodeType()).toEqual('default');
-			});
-		});
+        describe('Nodes', function () {
+            it('Can create node', function () {
+                expect(tsw.createNode().nodeType()).toEqual('default');
+            });
+        });
 
         describe('Browser Support', function () {
             it('Browser is supported', function () {
@@ -42,74 +41,73 @@ describe('Theresa\'s Sound World', function () {
         });
 
         describe('Create Buffer', function () {
-
             it('Node is a Buffer', function () {
                 expect(tsw.buffer().nodeType()).toEqual('buffer');
             });
         });
 
-		describe('Create Oscillator', function () {
+        describe('Create Oscillator', function () {
 
-			it('Node is an OscillatorNode', function () {
-				expect(tsw.oscillator().nodeType()).toEqual('oscillator');
-			});
+            it('Node is an OscillatorNode', function () {
+                expect(tsw.oscillator().nodeType()).toEqual('oscillator');
+            });
 
-			it('Default type is a sine wave', function () {
-				expect(tsw.oscillator().type()).toEqual('sine');
-			});
+            it('Default type is a sine wave', function () {
+                expect(tsw.oscillator().type()).toEqual('sine');
+            });
 
-			it('Creates a sine wave.', function () {
-				expect(tsw.oscillator('sine').type()).toEqual('sine');
-			});
+            it('Creates a sine wave.', function () {
+                expect(tsw.oscillator('sine').type()).toEqual('sine');
+            });
 
-			it('Creates a square wave.', function () {
-				expect(tsw.oscillator('square').type()).toEqual('square');
-			});
+            it('Creates a square wave.', function () {
+                expect(tsw.oscillator('square').type()).toEqual('square');
+            });
 
-			it('Creates a triangle wave', function () {
-				expect(tsw.oscillator('triangle').type()).toEqual('triangle');
-			});
+            it('Creates a triangle wave', function () {
+                expect(tsw.oscillator('triangle').type()).toEqual('triangle');
+            });
 
-			it('Creates a sawtooth wave', function () {
-				expect(tsw.oscillator('sawtooth').type()).toEqual('sawtooth');
-			});
+            it('Creates a sawtooth wave', function () {
+                expect(tsw.oscillator('sawtooth').type()).toEqual('sawtooth');
+            });
 
-			it('Creates a sine wave with a certain frequency', function () {
-				var osc = tsw.oscillator('sawtooth', 500);
-				expect(osc.frequency()).toEqual(500);
-			});
+            it('Creates a sine wave with a certain frequency', function () {
+                var osc = tsw.oscillator('sawtooth', 500);
+                expect(osc.frequency()).toEqual(500);
+            });
 
-			it('Sets the frequency', function () {
-				var osc = tsw.oscillator('sine');
-				osc.frequency.value = 900;
-				expect(osc.frequency.value).toEqual(900);
-			});
-		});
+            it('Sets the frequency', function () {
+                var osc = tsw.oscillator('sine');
+                osc.frequency.value = 900;
+                expect(osc.frequency.value).toEqual(900);
+            });
+        });
 
-		describe('Create Gain', function () {
+        describe('Create Gain', function () {
 
-			it('Node should be a GainNode', function () {
-				expect(tsw.gain().nodeType()).toEqual('gain');
-			});
+            it('Node should be a GainNode', function () {
+                expect(tsw.gain().nodeType()).toEqual('gain');
+            });
 
-			it('Default gain value should be 1', function () {
-				expect(tsw.gain().gain()).toEqual(1);
-			});
+            it('Default gain value should be 1', function () {
+                expect(tsw.gain().gain()).toEqual(1);
+            });
 
-			it('Create gain node with different gain level than default', function () {
-				expect(tsw.gain(0.5).gain()).toEqual(0.5);
-			});
+            it('Create gain node with different gain level than default', function () {
+                expect(tsw.gain(0.5).gain()).toEqual(0.5);
+            });
 
-			it('Create gain node and change gain level after creation', function () {
-				var volume = tsw.gain();
-				volume.gain(0.2);
-				expect(volume.gain()).toEqual(0.20000000298023224);
-			});
+            it('Create gain node and change gain level after creation', function () {
+                var volume = tsw.gain();
+                volume.gain(0.2);
+                expect(volume.gain()).toEqual(0.20000000298023224);
+            });
 
             it('Change gain at a specified time', function (done) {
                 var osc = tsw.oscillator(),
-                    vol = tsw.gain(0.2),
-                    mute = tsw.gain(0);
+                vol = tsw.gain(0.2),
+                mute = tsw.gain(0);
 
                 tsw.connect(osc, vol, mute, tsw.speakers);
                 osc.start(tsw.now());
@@ -117,32 +115,32 @@ describe('Theresa\'s Sound World', function () {
                 vol.gain(0.5, tsw.now() + 2);
 
                 setTimeout(function () {
-                    expect(vol.gain()).toEqual(0.5); 
+                    expect(vol.gain()).toEqual(0.5);
                     done();
                 }, 3000);
             });
-		});
+        });
 
-		describe('Create Delay', function () {
+        describe('Create Delay', function () {
 
-			it('Node should be a delay node', function () {
-				expect(tsw.wait().nodeType()).toEqual('delay');
-			});
+            it('Node should be a delay node', function () {
+                expect(tsw.wait().nodeType()).toEqual('delay');
+            });
 
-			it('Default delayTime value should be 1', function () {
-				expect(tsw.wait().delayTime()).toEqual(1);
-			});
+            it('Default delayTime value should be 1', function () {
+                expect(tsw.wait().delayTime()).toEqual(1);
+            });
 
-			it('Create delay node with different delayTime than default', function () {
-				expect(tsw.wait(0.5).delayTime()).toEqual(0.5);
-			});
+            it('Create delay node with different delayTime than default', function () {
+                expect(tsw.wait(0.5).delayTime()).toEqual(0.5);
+            });
 
-			it('Create delay node and change delayTime after creation', function () {
-				var delay = tsw.wait();
-				delay.delayTime(0.2);
-				expect(delay.delayTime()).toEqual(0.20000000298023224);
-			});
-		});
+            it('Create delay node and change delayTime after creation', function () {
+                var delay = tsw.wait();
+                delay.delayTime(0.2);
+                expect(delay.delayTime()).toEqual(0.20000000298023224);
+            });
+        });
 
         describe('Create Filter', function () {
 
@@ -156,17 +154,17 @@ describe('Theresa\'s Sound World', function () {
 
         describe('Create Envelope', function () {
             var osc = tsw.oscillator(),
-                volume = tsw.gain(),
-                mute = tsw.gain(0),
-                envelope = tsw.envelope({
-                    param: volume.params.gain,
-                    startLevel: 0,
-                    maxLevel: 1,
-                    sustainLevel: 0.6,
-                    attackTime: 2,
-                    decayTime: 1,
-                    autoStop: false
-                });
+            volume = tsw.gain(),
+            mute = tsw.gain(0),
+            envelope = tsw.envelope({
+                param: volume.params.gain,
+                startLevel: 0,
+                maxLevel: 1,
+                sustainLevel: 0.6,
+                attackTime: 2,
+                decayTime: 1,
+                autoStop: false
+            });
 
             tsw.connect(osc, volume, mute, tsw.speakers);
 
@@ -226,97 +224,97 @@ describe('Theresa\'s Sound World', function () {
 
         });
 
-		describe('Create Noise', function () {
+        describe('Create Noise', function () {
 
-			it('Node nodeType is "noise"', function () {
-				expect(tsw.noise().nodeType()).toEqual('noise');
-			});
+            it('Node nodeType is "noise"', function () {
+                expect(tsw.noise().nodeType()).toEqual('noise');
+            });
 
-			it('Default colour is "white"', function () {
-				expect(tsw.noise().color()).toEqual('white');
-			});
-		});
+            it('Default colour is "white"', function () {
+                expect(tsw.noise().color()).toEqual('white');
+            });
+        });
 
-		describe('Load files', function () {
+        describe('Load files', function () {
 
             var my_success;
 
-			it('Load some mp3s', function (done) {
-				tsw.load({
-					files: {
-						sampleOne: 'samples/tsw1.mp3',
-						sampleTwo: 'samples/tsw2.mp3',
-						sampleThree: 'samples/tsw3.mp3',
-					}
-				}, function (success) {
+            it('Load some mp3s', function (done) {
+                tsw.load({
+                    files: {
+                        sampleOne: 'samples/tsw1.mp3',
+                    sampleTwo: 'samples/tsw2.mp3',
+                    sampleThree: 'samples/tsw3.mp3',
+                    }
+                }, function (success) {
                     my_success = success;
                     done();
-				});
-			});
+                });
+            });
 
             it('mp3s should be loaded', function () {
                 expect(Object.keys(my_success).length).toEqual(3);
             });
 
-			it('Load some files that don\'t exist', function () {
-				tsw.load({
+            it('Load some files that don\'t exist', function () {
+                tsw.load({
                     files: {
                         sampleOne: 'samples/nope1.mp3',
-                        sampleTwo: 'samples/nope2.mp3',
-                        sampleThree: 'samples/nope3.mp3',
+                    sampleTwo: 'samples/nope2.mp3',
+                    sampleThree: 'samples/nope3.mp3',
                     }
-				}, function () {
+                }, function () {
                     // do nothing
-				}, function () {
+                }, function () {
                     //expect(true).toEqual(true);
                 });
-			});
-		});
+            });
+        });
 
-		describe('Fade In', function () {
+        describe('Fade In', function () {
 
-			it('Oscillator node fades in', function (done) {
-				var osc = tsw.oscillator(),
-					mute = tsw.gain(0);
+            it('Oscillator node fades in', function (done) {
+                var osc = tsw.oscillator(),
+                mute = tsw.gain(0);
 
-				expect(osc.output.gain.value).toEqual(1);
+            expect(osc.output.gain.value).toEqual(1);
 
-				tsw.connect(osc, mute, tsw.speakers);
-				osc
-					.start(tsw.now())
-					.fadeIn();
+            tsw.connect(osc, mute, tsw.speakers);
+            osc
+                .start(tsw.now())
+                .fadeIn();
 
-				osc.stop(tsw.now() + 3);
+            osc.stop(tsw.now() + 3);
 
-                setTimeout(function () {
-                    expect(osc.output.gain.value).toEqual(1);
-                    done();
-                }, 1000);
+            setTimeout(function () {
+                expect(osc.output.gain.value).toEqual(1);
+                done();
+            }, 1000);
 
-			});
-		});
+            });
+        });
 
-		describe('Fade Out', function () {
+        describe('Fade Out', function () {
 
-			it('Oscillator node fades out', function (done) {
-				var osc = tsw.oscillator(),
-					mute = tsw.gain(0);
+            it('Oscillator node fades out', function (done) {
+                var osc = tsw.oscillator(),
+                mute = tsw.gain(0);
 
-				expect(osc.output.gain.value).toEqual(1);
-				tsw.connect(osc, mute, tsw.speakers);
-				osc
-					.start(tsw.now())
-					.fadeOut();
+            expect(osc.output.gain.value).toEqual(1);
+            tsw.connect(osc, mute, tsw.speakers);
+            osc
+                .start(tsw.now())
+                .fadeOut();
 
-                setTimeout(function () {
-                    expect(osc.output.gain.value).toEqual(0);
-                    done();
-                }, 4000);
-			});
-		});
-	});
+            setTimeout(function () {
+                expect(osc.output.gain.value).toEqual(0);
+                done();
+            }, 4000);
+            });
+        });
+    });
 
-	describe('Effects', function () {
+    describe('Effects', function () {
 
         it('Create delay effect', function () {
             // tsw.delay();
@@ -329,32 +327,32 @@ describe('Theresa\'s Sound World', function () {
         it('Create tremolo effect', function () {
             // tsw.tremolo();
         });
-	});
+    });
 
-	describe('Music', function () {
-		it('Turns sharp note into equivalent flat note', function () {
-			expect(tsw.flat('A#')).toEqual('Bb');
-			expect(tsw.flat('G#')).toEqual('Ab');
-		});
+    describe('Music', function () {
+        it('Turns sharp note into equivalent flat note', function () {
+            expect(tsw.flat('A#')).toEqual('Bb');
+            expect(tsw.flat('G#')).toEqual('Ab');
+        });
 
-		it('Turns flat note into equivalent sharp note', function () {
-			expect(tsw.sharp('Db')).toEqual('C#');
-			expect(tsw.sharp('Ab')).toEqual('G#');
-		});
+        it('Turns flat note into equivalent sharp note', function () {
+            expect(tsw.sharp('Db')).toEqual('C#');
+            expect(tsw.sharp('Ab')).toEqual('G#');
+        });
 
-		it('Returns the frequency of a given note', function () {
-			expect(tsw.frequency('A4')).toEqual(440.00);
-			expect(tsw.frequency('a6')).toEqual(1760.00);
-			expect(tsw.frequency('B2')).toEqual(123.47082531403103);
-			expect(tsw.frequency('C#3')).toEqual(138.59131548843604);
-			expect(tsw.frequency('d#3')).toEqual(155.56349186104046);
-			expect(tsw.frequency('eb3')).toEqual(155.56349186104046);
-			expect(tsw.frequency('C')).toEqual(261.6255653005986);
-			expect(tsw.frequency('D#')).toEqual(311.12698372208087);
-			expect(tsw.frequency('eb')).toEqual(311.12698372208087);
-			expect(tsw.frequency('Not a note')).toEqual(false);
-			expect(tsw.frequency(123)).toEqual(false);
-		});
+        it('Returns the frequency of a given note', function () {
+            expect(tsw.frequency('A4')).toEqual(440.00);
+            expect(tsw.frequency('a6')).toEqual(1760.00);
+            expect(tsw.frequency('B2')).toEqual(123.47082531403103);
+            expect(tsw.frequency('C#3')).toEqual(138.59131548843604);
+            expect(tsw.frequency('d#3')).toEqual(155.56349186104046);
+            expect(tsw.frequency('eb3')).toEqual(155.56349186104046);
+            expect(tsw.frequency('C')).toEqual(261.6255653005986);
+            expect(tsw.frequency('D#')).toEqual(311.12698372208087);
+            expect(tsw.frequency('eb')).toEqual(311.12698372208087);
+            expect(tsw.frequency('Not a note')).toEqual(false);
+            expect(tsw.frequency(123)).toEqual(false);
+        });
 
         it('Get notes from given chord', function () {
             expect(tsw.chord('C', 'major')).toEqual(['C', 'E', 'G', 'C']);
@@ -365,15 +363,15 @@ describe('Theresa\'s Sound World', function () {
             expect(tsw.scale ('C', 'major')).toEqual([ 'C', 'D', 'E', 'F', 'G', 'A', 'B', 'C' ]);
             expect(tsw.scale('D', 'minor')).toEqual([ 'D', 'E', 'F', 'G', 'A', 'A#', 'C', 'D']);
         });
-	});
+    });
 
-	describe('Midi', function () {
-		it('Gets note name from given number', function () {
-	        expect(tsw.midiNote(0)).toEqual('C0');
-	        expect(tsw.midiNote(48)).toEqual('C4');
-	        expect(tsw.midiNote(30)).toEqual('F#2');
+    describe('Midi', function () {
+        it('Gets note name from given number', function () {
+            expect(tsw.midiNote(0)).toEqual('C0');
+            expect(tsw.midiNote(48)).toEqual('C4');
+            expect(tsw.midiNote(30)).toEqual('F#2');
             expect(tsw.midiNote(111)).toEqual('D#9');
-		});
+        });
 
         it('Gets midi number from given note name', function () {
             expect(tsw.midiNote('C0')).toEqual(0);
@@ -381,5 +379,5 @@ describe('Theresa\'s Sound World', function () {
             expect(tsw.midiNote('F#2')).toEqual(30);
             expect(tsw.midiNote('d#9')).toEqual(111);
         });
-	});
+    });
 });
