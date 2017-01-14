dd431f62c862f0a9d24c4b970ab9e8e2df277658
Stuart Memo <stuartmemo@gmail.com>
2017-01-14 12:01:33 +0000

    fix(pause): Remove context.suspend()
    This commit no longer suspends the audio context when pausing. This was causing
    issues in Firefox when setting position whilst playing.

--- dist/tsw.js
+++ dist/tsw.js
@@ -1,7 +1,7 @@
 /**
  * @name Theresa's Sound World
  * @description A JavaScript library for audio manipulation.
- * @version v0.5.3
+ * @version v0.8.0
  * @tutorial http://theresassoundworld.com
  * @author Stuart Memo
  * @license MIT
@@ -86,7 +86,7 @@ window.tsw = tsw;
 var helpers = require('./helpers');
 
 var tsw,
-    version = '0.5.3';
+    version = '0.8.0';
 
 tsw = (function () {
 
--- src/tsw-core.js
+++ src/tsw-core.js
@@ -12,7 +12,7 @@
 var helpers = require('./helpers');
 
 var tsw,
-    version = '0.5.3';
+    version = '0.8.0';
 
 tsw = (function () {
 
